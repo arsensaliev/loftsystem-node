1e5669dd9b42376441157a29a9770311ff35a59a
Arsen Saliev <arsensaliev03@gmail.com>
2020-04-17 19:07:48 +0600

    fix bug

---
+++ build/precache-manifest.6ec87373e6fa43864cac9730fd100cb6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "438c9bf0ef50b7dc4478317211302242",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8aa0cbcd4e1deb053bc2",
+    "url": "/static/js/2.bf375665.chunk.js"
+  },
+  {
+    "revision": "f982f1070023455b9dc7",
+    "url": "/static/js/main.7b4fb9c4.chunk.js"
+  },
+  {
+    "revision": "32996fb276b3e890a01d",
+    "url": "/static/js/runtime~main.01574ed8.js"
+  }
+]);
